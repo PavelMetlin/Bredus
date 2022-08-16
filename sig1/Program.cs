1c4adbd101d7c462c37fa08606a2915a408a5aed
PavelMetlin <metlin-1@mail.ru>
2022-08-16 08:51:09 +0300

    initial

---
+++ sig1/Program.cs
@@ -0,0 +1,18 @@
+﻿// задача 1
+int degree(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int step = degree(numA, numB);
+Console.WriteLine("Ответ: " + step);
---
+++ sig2/Program.cs
@@ -0,0 +1,22 @@
+﻿// задача 2
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int num)
+{
+
+    int counter = Convert.ToString(num).Length;
+    int prog = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        prog = num - num % 10;
+        result = result + (num - prog);
+        num = num / 10;
+    }
+    return result;
+}
+
+int sumNum = SumNum(num);
+Console.WriteLine("Сумма цифр в числе " + sumNum);
